d560f06f477a63568b0a9218e2955227da9bc544
Zhao Ying Li <zhao.li@uwaterloo.ca>
2016-02-10 00:35:43 -0500

    Adding default files/folders to the project
    Using the default multiple view template provided by xcode.

---
+++ Moments/DataViewController.h
@@ -0,0 +1,17 @@
+//
+//  DataViewController.h
+//  Moments
+//
+//  Created by Nancy Li on 2016-02-10.
+//  Copyright © 2016 Nancy Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DataViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *dataLabel;
+@property (strong, nonatomic) id dataObject;
+
+@end
+
---
+++ Moments/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Moments
+//
+//  Created by Nancy Li on 2016-02-10.
+//  Copyright © 2016 Nancy Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
