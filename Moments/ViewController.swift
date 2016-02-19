d10a29912690c53a1f4cbdc08fa4bffef4573643
Yuning Xue <y28xue@uwaterloo.ca>
2016-02-18 20:43:28 -0500

    Initial Commit

---
+++ Moments/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Moments
+//
+//  Created by Yuning Xue on 2016-02-18.
+//  Copyright © 2016 Moments. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
