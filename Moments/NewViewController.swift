6c03bbc71284bf14c274eec0123eba6cbfcf5b41
NancyZhaoLi <zhao.li@uwaterloo.ca>
2016-02-19 21:35:57 -0500

    Adding gitignore and updating the new view to have selections for text/image/etc

---
+++ Moments/NewViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewViewController.swift
+//  Moments
+//
+//  Created by Nancy Li on 2016-02-19.
+//  Copyright © 2016 Moments. All rights reserved.
+//
+
+import UIKit
+
+class NewViewController: UIViewController {
+    
+    @IBAction func addItem(sender: AnyObject) {
+        print(sender.title)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        print("New moment loaded")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
