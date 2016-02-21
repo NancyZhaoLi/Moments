4502a08f92cfc4c7471eec12ce834277d057fa61
Xin Lin <xlin@Xins-iMac.local>
2016-02-21 08:02:57 -0800

    added untested audio and video code, bring your phone, please

---
+++ AudioPlayerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AudioPlayerViewController.swift
+//  Moments
+//
+//  Created by Xin Lin on 2016-02-21.
+//  Copyright © 2016 Moments. All rights reserved.
+//
+
+import UIKit
+
+class AudioPlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
