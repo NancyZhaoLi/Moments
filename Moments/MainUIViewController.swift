d3db1ab9929c513811fb5145fd8f1d33db861cd1
monica <lalaphoon@gmail.com>
2016-02-21 18:45:42 -0500

    add MainUIViewController

---
+++ Moments/MainUIViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MainUIViewController.swift
+//  Moments
+//
+//  Created by Mengyi LUO on 2016-02-21.
+//  Copyright © 2016 Moments. All rights reserved.
+//
+
+import UIKit
+
+class MainUIViewController: UITabBarController {
+
+}
