af23cf151369ba09870948078739cd72857794ce
NancyZhaoLi <nancy_zhaoying_li@hotmail.com>
2016-02-24 10:49:40 -0500

    add popup date selector for moments mode

--- Moments/MomentsViewController.swift
+++ Moments/MomentsViewController.swift
@@ -10,14 +10,33 @@ import UIKit
 
 class MomentsViewController: UIViewController {
 
+    @IBOutlet weak var startDate: UITextField!
+
     
-    @IBAction func setStartDate(sender: UIButton) {
-    }
+    @IBOutlet weak var endDate: UITextField!
+
     
-    
-    @IBAction func setEndDate(sender: AnyObject) {
+    @IBAction func startDate(sender: UITextField) {
+        print("clikc")
+        var datePickerView: UIDatePicker = UIDatePicker()
+        datePickerView.datePickerMode = UIDatePickerMode.Date
+        sender.inputView = datePickerView
+        datePickerView.addTarget(self, action: Selector("handleDatePicker:"), forControlEvents: UIControlEvents.TouchUpInside)
         
     }
     
+    func handleDatePicker(sender: UIDatePicker){
+        var timeFormatter = NSDateFormatter()
+        timeFormatter.dateStyle = .NoStyle
+        timeFormatter.timeStyle = .NoStyle
+        startDate.text = timeFormatter.stringFromDate(sender.date)
+    }
     
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func endDate(sender: AnyObject) {
+    }
 }
