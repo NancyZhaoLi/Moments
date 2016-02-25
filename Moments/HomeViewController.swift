53ba79ebcd22fa82c3cffc64f112e5b9b5cde400
Yuning Xue <y28xue@uwaterloo.ca>
2016-02-24 23:14:45 -0500

    add table view in Home and Calendar

--- Moments/HomeViewController.swift
+++ Moments/HomeViewController.swift
@@ -9,17 +9,20 @@
 import UIKit
 import CoreData
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDelegate {
     
+    var content = ["Rob", "K", "E"]
     var momentsMO = [Moment]()
     var moments = [MomentEntry]()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         print("home loaded")
         getMomentsFromCoreData()
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -51,7 +54,6 @@ class HomeViewController: UIViewController {
     
     func getMomentsMOFromCoreData(){
         momentsMO = CoreDataFetchHelper.fetchMomentsMOFromCoreData()
-        
     }
     
     func addMomentFromCoreData(momentMO: Moment) {
@@ -63,15 +65,17 @@ class HomeViewController: UIViewController {
         moments.append(moment)
         print("id: \(id!), date: \(date!), title: \(title!)")
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return content.count
     }
-    */
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = content[indexPath.row]
+        
+        return cell
+    }
 
 }
