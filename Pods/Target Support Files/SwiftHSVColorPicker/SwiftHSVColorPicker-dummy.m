4a105845536760d93b31c249df0ce19801d724aa
Yuning Xue <y28xue@uwaterloo.ca>
2016-02-26 04:21:07 -0500

    add day view moments table

---
+++ Pods/Target Support Files/SwiftHSVColorPicker/SwiftHSVColorPicker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SwiftHSVColorPicker : NSObject
+@end
+@implementation PodsDummy_SwiftHSVColorPicker
+@end
---
+++ Pods/Target Support Files/SwiftHSVColorPicker/SwiftHSVColorPicker-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double SwiftHSVColorPickerVersionNumber;
+FOUNDATION_EXPORT const unsigned char SwiftHSVColorPickerVersionString[];
+
