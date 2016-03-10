6b218127ed3b1ea31bd4d740944ed30bf75c75fc
monica <lalaphoon@gmail.com>
2016-03-10 16:55:56 -0500

    AboutUs: Bug need to be fixed:can't open multiple webpages

--- aboutusViewController.swift
+++ aboutusViewController.swift
@@ -22,12 +22,87 @@ class aboutusViewController: UIViewController {
     }
     
 
-    @IBAction func monicalink(sender: AnyObject) {
-        
+    @IBAction func nancylink(sender: UIButton) {
+           // if sender.
+        if( sender.currentImage! == UIImage(named:"linkdin_f.png")){
+        print(sender)
         UIApplication.sharedApplication().openURL(NSURL(string: "https://www.linkedin.com/in/nancy-zhao-ying-li-9b353496")!)
+        }
+        else if (sender.currentImage! == UIImage(named:"github_f.png")) {
+           
+            
+            
+            // print("havn't added")
+            UIApplication.sharedApplication().openURL(NSURL(string: "https://github.com/NancyZhaoLi")!)
+           
+        
+        }
         
         
     }
+    
+     @IBAction func xinlink(sender: UIButton) {
+    
+        if( sender.currentImage! == UIImage(named:"linkdin_f.png")){
+            print(sender)
+            UIApplication.sharedApplication().openURL(NSURL(string: "https://www.linkedin.com/in/xin-lin-7a5872b5")!)
+        }
+        else if (sender.currentImage! == UIImage(named:"github_f.png")) {
+            
+            
+            
+            // print("havn't added")
+            UIApplication.sharedApplication().openURL(NSURL(string: "https://github.com/x58lin")!)
+            
+            
+        }
+    
+    }
+    
+    
+    
+     @IBAction func emilylink(sender: UIButton) {
+    
+        if( sender.currentImage! == UIImage(named:"linkdin_f.png")){
+            print(sender)
+            UIApplication.sharedApplication().openURL(NSURL(string: "https://www.linkedin.com/in/yuning-xue-a5920a62")!)
+        }
+        else if (sender.currentImage! == UIImage(named:"github_f.png")) {
+            
+            
+            
+            // print("havn't added")
+            UIApplication.sharedApplication().openURL(NSURL(string: "https://github.com/xyuning")!)
+            
+            
+        }
+    
+    }
+    
+    
+    
+     @IBAction func monicalink(sender: UIButton) {
+    
+        if( sender.currentImage! == UIImage(named:"twitter_f.png")){
+            print(sender)
+            UIApplication.sharedApplication().openURL(NSURL(string: "https://twitter.com/lalaphoon")!)
+        }
+        else if (sender.currentImage! == UIImage(named:"github_f.png")) {
+            
+            
+            
+            // print("havn't added")
+            UIApplication.sharedApplication().openURL(NSURL(string: "https://github.com/lalaphoon")!)
+            
+            
+        }
+    
+    }
+    
+    
+    
+    
+    
     /*
     // MARK: - Navigation
 
