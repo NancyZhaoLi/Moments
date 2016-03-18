dcb7b9c4c597e036b4841f34b679e58e3eedd548
x58lin <x58lin>
2016-03-17 21:59:39 -0400

    category and moment relation changed to regular set not orderd

--- Moments/Category+CoreDataProperties.swift
+++ Moments/Category+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Category+CoreDataProperties.swift
 //  Moments
 //
-//  Created by Xin Lin on 2016-03-16.
+//  Created by Mengyi LUO on 2016-03-17.
 //  Copyright © 2016 Moments. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
@@ -16,6 +16,6 @@ extension Category {
 
     @NSManaged var colour: NSObject?
     @NSManaged var name: String?
-    @NSManaged var containedMoment: NSOrderedSet?
+    @NSManaged var containedMoment: NSSet?
 
 }
