cfe387979ca8dcabce10e9c7a69bff9f62daaee6
Yuning Xue <y28xue@uwaterloo.ca>
2016-03-22 05:35:27 -0400

    change data model: add id for category

--- Moments/Category+CoreDataProperties.swift
+++ Moments/Category+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Category+CoreDataProperties.swift
 //  Moments
 //
-//  Created by Mengyi LUO on 2016-03-17.
+//  Created by Yuning Xue on 2016-03-22.
 //  Copyright © 2016 Moments. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
@@ -16,6 +16,7 @@ extension Category {
 
     @NSManaged var colour: NSObject?
     @NSManaged var name: String?
+    @NSManaged var id: NSNumber?
     @NSManaged var containedMoment: NSSet?
 
 }
